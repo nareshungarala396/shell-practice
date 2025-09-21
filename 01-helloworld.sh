e5b8641a8aa25410b252d159106920309bddb074
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-21 15:13:34 +0530

    naresh suresh

---
+++ 01-helloworld.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Person1 = ramesh
+person2 = suresh
+
+echo Person1 : Hi $Person2
+echo Person2 : Hello $Person1
---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+Person1=naresh
+Person2=suresh
+
+echo "$Person1: hello $person2"
+echo "$Person2: hi $Peson1"
