9721129be1ef3c06d128fc9058be8277c9271add
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-22 21:03:07 +0530

    helloworld

--- 10-even.sh
+++ 10-even.sh
@@ -2,10 +2,9 @@
 
 echo "provide NUMBER:"
 read NUMBER
-
-
-if [ $(( $NUMBER%2 -eq 0 ))] ; then
-    echo "$NUMBER is even number"
+if [ $(($NUMBER % 2 -eq 0)) ]; then
+    echo $NUMBER is EVEN
 else
-    echo "$NUMBER is odd"
-fi        
+    echo $NUMBER is odd
+
+fi
