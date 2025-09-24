94dbc7aa3f4312d00499ea59a0f0a9c3347c8867
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-24 13:20:35 +0530

    helloworld

---
+++ 14-colours2.sh
@@ -0,0 +1,50 @@
+#!bin/bash
+
+USERID=$(u -id)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0]; then
+    echo -e "please use root Privilages...$R Error $N"
+    exit 1
+fi
+
+VALIDATION(){
+    if [ $1 -ne 0 ];then
+        echo -e "installation of mysql is ...$R failed $N"
+    else
+        echo -e "installation completed ...$G successfull $N"
+    fi    
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else 
+    echo -e "required software is already...$G installed $N"
+
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else 
+    echo -e "required software is already...$G installed $N"
+
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else 
+    echo -e "required software is already...$G installed $N"
+
+fi
