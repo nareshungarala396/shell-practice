29620e65040e81241d5187769f3351c86db9fc18
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-27 21:05:05 +0530

    helloworld

---
+++ 17-loops2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+R="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo "$0 | cut -d "." -f1")
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+
+if [ $USERID -ne 0]; then
+    echo -e "please use $R root user privilages $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "installation of $2 has been $R failed $N" | tee -a $LOG_FILE
+    else
+    echo -e "installation of $2 has been $G successfull $N" | tee -a $LOG_FILE
+    if
+    }
+
+for package in $@
+do
+
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0] ; then
+        echo -e "required $package $R not istalled yet $N"
+
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else
+        echo "required $package already exist"
+    fi
+done
