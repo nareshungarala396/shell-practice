f3d8efe3d1af451de400d3c9d64b825c3e79d8f3
nareshungarala396 <nareshungarala220@gmail.com>
2025-09-27 14:16:19 +0530

    helloworld

--- 15-logs.sh
+++ 15-logs.sh
@@ -7,7 +7,7 @@ Y="\e[33m"
 N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-practice"
-SCRIPT_NAME=$((echo $0 | cut -d "." -f1))
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOGS_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
@@ -15,4 +15,43 @@ mkdir -p $LOG_FOLDER
 if [ $USERID -ne 0 ]; then
     echo -e "please use root Privilages...$R Error $N"
     exit 1
+fi
+
+VALIDATION(){
+    if [ $1 -ne 0 ]; then
+        echo -e "installation of $2 is ...$R failed $N"
+        exit 1
+    else
+        echo -e "installation completed of $2...$G successfull $N"
+    fi    
+}
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATION $? "MYSQL"
+else 
+    echo -e "required software is already...$G installed $N"
+
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATION $? "Nginx"
+else 
+    echo -e "required software is already...$G installed $N"
+
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATION $? "python3"
+else 
+    echo -e "required software is already...$G installed $N"
+
 fi
